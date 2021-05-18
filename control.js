9c47111661e63cf8ca6e40867fec526c54c33aa8
sonamzah <ztsonam@gmail.com>
2021-05-17 17:38:07 -0700

    most of this project is already done but I wanted to save some of my trials and tribulations before I clean up the markup!

---
+++ control.js
@@ -0,0 +1,13 @@
+//** MODEL **/
+// Questions
+const q0 = '';
+const q1 = '';
+const q2 = '';
+const q3 = '';
+const q4 = '';
+const state = {
+    questions =[q0, q1, q2, q3, q4],
+    answers =[],
+}
+//** VIEW **/  
+//** CONTROLLER **/
